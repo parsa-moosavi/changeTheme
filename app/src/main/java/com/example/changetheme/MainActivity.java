39daef6ab4d5c1d21304ce303d91b618046c0efe
parsa-moosavi <63161776+parsa-moosavi@users.noreply.github.com>
2021-10-04 07:12:51 +0330

    I just fucking did it

--- app/src/main/java/com/example/changetheme/MainActivity.java
+++ app/src/main/java/com/example/changetheme/MainActivity.java
@@ -16,12 +16,15 @@ import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
     LinearLayout linearLayout;
+    TextView textView;
     Button button;
+    boolean isDark;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,15 +32,17 @@ public class MainActivity extends AppCompatActivity {
         imageView = findViewById(R.id.imageView);
         linearLayout = findViewById(R.id.container);
         button = findViewById(R.id.button);
+        textView = findViewById(R.id.txt);
         button.setOnClickListener(v -> {
          setTheme(null);
         });
+        isDark=false;
     }
 
 
     public void setTheme(Resources.Theme theme) {
         if (imageView.getVisibility()== View.VISIBLE) {
-            return;
+//            return;
         }
 
         int w = linearLayout.getWidth();
@@ -48,18 +53,38 @@ public class MainActivity extends AppCompatActivity {
         linearLayout.draw(canvas);
 
         imageView.setImageBitmap(bitmap);
+//        imageView.setImageResource(R.drawable.ic_launcher_background);
         imageView.setVisibility(View.VISIBLE);
 
-        float finalRadius = (float) Math.hypot(w,h);
+        float finalRadius = (float) Math.hypot((float)w,(float)h);
 
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-
-        Animator anim = ViewAnimationUtils.createCircularReveal(linearLayout, w / 2, h / 2, 0f, finalRadius);
+//        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        if (isDark){
+            linearLayout.setBackgroundColor(getResources().getColor(R.color.white));
+            textView.setTextColor(getResources().getColor(R.color.black));
+            isDark=false;
+        }
+        else {
+            linearLayout.setBackgroundColor(getResources().getColor(R.color.black));
+            textView.setTextColor(getResources().getColor(R.color.white));
+            isDark=true;
+        }
+//        linearLayout.animate().translationX(200).setDuration(1000);
+        Animator anim = ViewAnimationUtils.createCircularReveal(linearLayout, button.getLeft()+button.getWidth()/2, button.getTop()+button.getHeight()/2, 0f, finalRadius);
         anim.setDuration(400L);
-//        anim.doOnEnd {
-//            imageView.setImageDrawable(null);
-//            imageView.setVisibility(View.GONE);
-//        }
+        anim.addPauseListener(new Animator.AnimatorPauseListener() {
+            @Override
+            public void onAnimationPause(Animator animation) {
+                imageView.setImageDrawable(null);
+                imageView.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onAnimationResume(Animator animation) {
+                imageView.setImageDrawable(null);
+                imageView.setVisibility(View.GONE);
+            }
+        });
         anim.start();
     }
 }
